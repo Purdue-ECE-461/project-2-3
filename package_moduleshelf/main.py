71bf8b599ce5375376bf04379d7aef4987fc1c79
rober423 <rober423@purdue.edu>
2021-11-15 11:46:18 -0500

    change file organization, api instance inside shelf main

--- package_moduleshelf/main.py
+++ package_moduleshelf/main.py
@@ -21,6 +21,15 @@ from google.cloud import error_reporting
 import google.cloud.logging
 import storage
 
+from flask_restful import Resource, Api, request, reqparse
+import pandas as pd
+import ast
+
+import Packages
+import Reset
+import Package
+import PackageRating
+import Authenticate
 
 # [START upload_image_file]
 def upload_image_file(img):
@@ -54,6 +63,16 @@ app.config.update(
 
 app.debug = False
 app.testing = False
+
+api = Api(app)
+api.add_resource(Packages, '/packages')
+api.add_resource(Reset, '/reset')
+api.add_resource(Packages.get_package_by_ID(id), '/package/{id}')
+api.add_resource(Package, '/package')
+api.add_resource(PackageRating.rate_by_ID(id), '/package/{id}/rate')
+api.add_resource(Authenticate, '/authenticate')
+api.add_resource(Packages.get_package_by_Name(Name), '/package/byName/{Name}')
+api.add_resource(Packages.delete_package_by_Name(Name), '/package/byName/{Name}')
 
 # Configure logging
 if not app.testing:
