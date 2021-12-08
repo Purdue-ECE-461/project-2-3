29fcaf0261824bfa84e34b0ddeae6a69b79c1ad1
Parker Bushey <42950212+edapaker@users.noreply.github.com>
2021-12-08 17:34:06 -0500

    Update ZipUnzip.py
    Remove extra copy of clean method

--- api/ZipUnzip.py
+++ api/ZipUnzip.py
@@ -46,23 +46,7 @@ class ZipUnzip :
             except Exception as e:
                 print('Failed to delete %s. Reason: %s' % (file_path, e))
         os.rmdir(folder)
-        
-    # This code pulled from https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder
-    def clean (self) :
-        folder = 'packageTemp'
-
-        for filename in os.listdir(folder) :
-            file_path = os.path.join(folder, filename)
-
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-            except Exception as e:
-                print('Failed to delete %s. Reason: %s' % (file_path, e))
-        os.rmdir(folder)
-
+       
     def file_zip (self) :
         for directory in os.walk("packageTemp") :
             entry = directory
