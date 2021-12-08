2645cafa574da28d9261fd2c8f176101d70417ca
Parker Bushey <42950212+edapaker@users.noreply.github.com>
2021-12-08 17:55:24 -0500

    Package audit var names updated

--- api/PackageAudit.py
+++ api/PackageAudit.py
@@ -2,15 +2,15 @@ import os
 
 class PackageAudit (object) :
 
-    package_name = ""
+    package_filepath = ""
     destination_folder = ""
 
-    def __init__ (self, _package_name, _destination_folder) :
-        self.package_name = _package_name
+    def __init__ (self, _package_filepath, _destination_folder) :
+        self.package_filepath = _package_filepath
         self.destination_folder = _destination_folder
 
     def audit_run (self) :
-        os.system("cd " + self.package_name + " & npm audit --json | npm-audit-html --output " + self.destination_folder + "auditReport.html")
+        os.system("cd " + self.package_filepath + " & npm audit --json | npm-audit-html --output " + self.destination_folder + "auditReport.html")
 
 if __name__ == "__main__" :
     test = PackageAudit("mocha-master", "../")
