f9671a73d10160a1f8dd35f85b8249c5ca81811d
Ashwin Senthilkumar <ashsenth@gmail.com>
2021-12-09 01:52:32 -0500

    added Semantic versioning

--- api/SemverRange.py
+++ api/SemverRange.py
@@ -1,3 +1,26 @@
+import package_moduleshelf
+import semver
+
 
 class SemverRange(object):
-    
+    #assuming object is txt file
+    file1 = open('requirements.txt', 'r')
+    count = 0 #line number
+
+    while True:
+        verMajor = None
+        verMinor = None
+        verPatch = None
+
+        lines = file1.readlines()
+        if not lines:
+            break
+
+        for x in lines:
+            if lines[count][x] == "=" & lines[count][x + 1] == "=":
+                ver = semver.Version.parse(lines[count][x:])
+                verMajor = ver.major
+                verMinor = ver,minor
+                verPatch = ver.patch             
+
+        count += 1
